7c14a25609cb5f6f032c6b540a173eac316378d0
ismahmjhdh <89400479+ismahmjhdh@users.noreply.github.com>
2025-12-23 20:52:29 +0700

    menghapus folder lama

--- BACKEND/websitekreasimu/resources/views/materi-detail.blade.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="/materi/{{ $materi->id }}/verify">
-    @csrf
-    <input type="password" name="password" placeholder="Masukkan password">
-    <button type="submit">Akses Materi</button>
-</form>
